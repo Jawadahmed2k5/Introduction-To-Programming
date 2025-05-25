2c144f80c017aecfbc6a95004188702dbb2a68ae
Jawad Ahmed <officialjawad25@gmail.com>
2025-05-25 11:54:52 +0500

    lecture no 4 and problem solved

---
+++ sepratingnumbers.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+main(){
+int number;
+int digit;
+
+cout << "Please  enter a 4 digits integor: ";
+cin >> number;
+
+digit = number % 10;
+cout << "The Digit is: "<< digit << "\n";
+number = number / 10;
+digit = number % 10;
+
+digit = number % 10;
+cout << "The Digit is: "<< digit << "\n";
+number = number / 10;
+digit = number % 10;
+
+digit = number % 10;
+cout << "The Digit is: "<< digit << "\n";
+number = number / 10;
+digit = number % 10;
+
+digit = number % 10;
+cout << "The Digit is: "<< digit << "\n";
+number = number / 10;
+digit = number % 10;
+}
